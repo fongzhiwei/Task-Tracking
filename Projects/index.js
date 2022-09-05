168cbe59af99fd136da1ced6ffe9078eabb180c8
syen9 <jamieyeow1203@gmail.com>
2022-09-05 23:00:25 +0800

    Update Edit Function

--- Projects/index.js
+++ Projects/index.js
@@ -1,47 +1,47 @@
-//getting task conatainer to add cards
-const taskContainer = document.querySelector(".task__container");
+//getting task conatainer to add tasks
+const taskCreation = document.querySelector(".task_creation");
 
 // new task template
-const generateNewCard = (taskData) => `
+const generateNewTask = (taskData) => `
 <div class="col-md-6 col-lg-4 mt-3" >
 <div class="card shadow-sm task__card">
 
   <div class="card-header d-flex justify-content-end task__card__header">
-    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteCard.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
-                  <i class="fas fa-trash-alt"id=${taskData.id}
-                  data-bs-target="#animateModal" data-bs-toggle="modal" onclick="deleteCard.apply(this, arguments)" ></i>
-                </button>
+    <button type="button" class="btn btn-outline-info mr-2" onclick ="editTask()">
+      <i class="fas fa-pencil-alt" onlick ="editTask()"></i>
+    </button>
 
+    <button type="button" class="btn btn-outline-danger" id=${taskData.id} onclick="deleteTask.apply(this, arguments)" data-bs-toggle="modal">
+      <i class="fas fa-trash-alt"id=${taskData.id} data-bs-toggle="modal" onclick="deleteTask.apply(this, arguments)"></i>
+    </button>
   </div>
+
   <div class="card-body">
     <h2 class="task__card__name" id="name">${taskData.taskName}</h2>
-    <p class="description trim-3-lines text-muted" id="desc" >
-     ${taskData.taskDesc}
-    </p>
-    <div class="tags text-white d-flex flex-wrap">
-      <span class="badge bg-primary m-1" id="type">${taskData.taskPriority}</span>
-    </div>
-    <div class="tags text-white flex-wrap">
-      <span class="badge bg-primary m-1" id="type">${taskData.taskType}</span>
-    </div>
+      <p class="description trim-3-lines text-muted" id="desc" >${taskData.taskDesc}</p>
+      <div class="tags text-white d-flex flex-wrap"><span class="badge bg-primary m-1" id="priority">${taskData.taskPriority}</span></div>
+      <div class="tags text-white flex-wrap"><span class="badge bg-primary m-1" id="type">${taskData.taskType}</span>
   </div>
-  <div class="card-footer"></div>
-</div>
-</div>
 
+  <div class="card-footer">
+    <button type="button" onclick="saveEdit()" class="btn btn-primary btn-default active">Save Changes</button>
+  </div>
+</div>
+</div>
 `;
 
-let globalStorage = []; // declaring an empty array to store data
+// declaring an empty array to store data
+let globalStorage = [];
 
-// get task data on load/refreah from local storage
-const loadCardData = () => {
-  const getCardData = localStorage.getItem("tasky");
-  const { cards } = JSON.parse(getCardData);
+// get task data from local storage
+const loadTaskData = () => {
+  const getTaskData = localStorage.getItem("task");
+  const {task} = JSON.parse(getTaskData);
 
-  cards.map((cardObject) => {
-    taskContainer.insertAdjacentHTML("beforeend", generateNewCard(cardObject));
+  task.map((taskObject) => {
+    taskCreation.insertAdjacentHTML("beforeend", generateNewTask(taskObject));
 
-    globalStorage.push(cardObject);
+    globalStorage.push(taskObject);
   });
 };
 
@@ -54,34 +54,57 @@ const saveChanges = () => {
     taskPriority: document.getElementById("taskPriority").value,
     taskDesc: document.getElementById("taskDesc").value,
   };
-  taskContainer.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  taskCreation.insertAdjacentHTML("beforeend", generateNewTask(taskData));
 
   globalStorage.push(taskData);
 
-  localStorage.setItem("tasky", JSON.stringify({ cards: globalStorage }));
+  localStorage.setItem("task", JSON.stringify({task: globalStorage}));
 };
 
-//delete task
-const deleteCard = (event) => {
+// delete task
+const deleteTask = (event) => {
   event = window.event;
   const targetId = event.target.id;
   const tagName = event.target.tagName;
 
   globalStorage = globalStorage.filter(
-    (cardObject) => cardObject.id != targetId
+    (taskObject) => taskObject.id != targetId
   );
 
-  localStorage.setItem("tasky", JSON.stringify({ cards: globalStorage }));
+  localStorage.setItem("task", JSON.stringify({task: globalStorage}));
 
   if (tagName == "BUTTON") {
-    return taskContainer.removeChild(
+    return taskCreation.removeChild(
       event.target.parentNode.parentNode.parentNode
     );
   } else {
-    return taskContainer.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    return taskCreation.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
   }
 };
 
+// edit task
+const editTask = () => {
+  document.getElementById("name").contentEditable = "true";
+  document.getElementById("type").contentEditable = "true";
+  document.getElementById("desc").contentEditable = "true";
+  document.getElementById("priority").contentEditable = "true";
+};
 
+const saveEdit = () => {
+  const editName = document.getElementById("name").textContent;
+  const editType = document.getElementById("type").textContent;
+  const editDesc = document.getElementById("desc").textContent;
+  const editPrior = document.getElementById("priority").textContent;
+
+  document.getElementById("name").innerHTML = editName;
+  document.getElementById("type").innerHTML = editType;
+  document.getElementById("desc").innerHTML = editDesc;
+  document.getElementById("priority").innerHTML = editPrior;
+
+  document.getElementById("name").contentEditable = "false";
+  document.getElementById("desc").contentEditable = "false";
+  document.getElementById("type").contentEditable = "false";
+  document.getElementById("priority").contentEditable = "false";
+};
 
 
