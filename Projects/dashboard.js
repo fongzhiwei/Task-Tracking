af04c7990072062a5c2d7728cf48cd61d7851a9a
zfon0005 <zfon0005@student.monash.edu>
2022-09-10 04:58:44 +0800

    filter function finalization commit

--- Projects/dashboard.js
+++ Projects/dashboard.js
@@ -1,3 +1,7 @@
+const filterTasks = document.querySelector(".filter-task")
+
+filterTasks.addEventListener("click",filterFunction)
+
 function searchFunction() {
   // Declare variables
   var input, filter;
@@ -16,3 +20,29 @@ function searchFunction() {
     }
   }
 }
+
+function filterFunction() {
+    // Declare variables
+    var input, filter;
+    var selected = document.getElementById('taskFilter');
+    input = selected.options[selected.selectedIndex];
+    filter = input.text.toUpperCase();
+    cards = document.getElementsByClassName("displayTaskSlot");
+    titles = document.querySelectorAll('[id=status]');
+  
+    if(filter == "FILTER"){
+        for (i=0; i<cards.length; i++){
+            cards[i].style.display = "";
+        }
+        return
+    }
+    // Loop through all list items, and hide those tasks that don't match the filter
+    for (i = 0; i < cards.length; i++) {
+      a = titles[i];
+      if (a.innerHTML.toUpperCase().indexOf(filter) > -1) {
+        cards[i].style.display = "";
+      } else {
+        cards[i].style.display = "none";
+      }
+    }
+  }
