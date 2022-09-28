28c8c97fddb4c7185cb716d7296d8074b2f4148a
Ng Yu Mei <yngg0068@student.monash.edu>
2022-09-29 02:22:10 +0800

    Create function to change the status of sprint

--- Projects/sprintCreate.js
+++ Projects/sprintCreate.js
@@ -1,15 +1,16 @@
+
 const sprintCreation = document.querySelector(".display");
 
 const generateNewCard = (sprint) => `<div class = "card">
 <div>
     <div id="upperLine">
         <h5 id="taskName"> <b> ${sprint.sprintTitle} </b></h5>
-        <button type="button" class="btn btn-outline-danger" id=${sprint.id} onclick="deleteTask.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
+        <button type="button" class="btn btn-outline-danger" id=${sprint.id} class="asa" onclick="deleteTask.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
         <i class="fas fa-trash-alt"id=${sprint.id}
         data-bs-target="#animateModal" data-bs-toggle="modal" onclick="deleteTask.apply(this, arguments)" ></i>
       </button>
       <div id="statusButtons"> 
-        <div><button id="status" onclick="statusButton()">  <a type="button" href="login.html"> Get Started </a> </button></div> 
+        <div><button id=${sprint.id1} onclick="statusButton.apply(this, arguments)">Get Started</button></div> 
         <div><button type="button" id="finish" onclick="" > Finished </button> </div>
          </div>
         </div>
@@ -29,22 +30,27 @@ const loadTaskData = () => {
 
   sprintTask.map((taskObject) => {
     sprintCreation.insertAdjacentHTML("beforeend", generateNewCard(taskObject));
+    const b = document.getElementById(taskObject.id1);
+    b.innerText = localStorage.getItem(taskObject.id1);
 
     globalStorage.push(taskObject);
+    
   });
+
 };
 
 // save changes of new task 
 const saveChanges = () => {
   const taskData = {
     id: Date.now(),
+    id1 : Date.now()+"1",
     sprintTitle: document.getElementById("sprintTitle").value, 
     startDate: document.getElementById("startDate").value,
   };
   sprintCreation.insertAdjacentHTML("beforeend", generateNewCard(taskData));
+  localStorage.setItem(taskData.id1, "Get Started");
 
   globalStorage.push(taskData);
-
   localStorage.setItem("sprint", JSON.stringify({sprintTask: globalStorage}));
 };
 
@@ -69,8 +75,11 @@ const deleteTask = (event) => {
   }
 };
 
-const statusButton = ()=>{
-   const button1 = document.getElementById("status");
-   button1.innerText = "Contitue";
+const statusButton = (event)=>{
+   event = window.event;
+   const statusId = event.target.id;
+   localStorage.setItem(statusId, "Continue");
+   const button1 = document.getElementById(statusId);
+   button1.innerText = localStorage.getItem(statusId);
 
 }
