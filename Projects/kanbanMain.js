814a26ee98b81fb2617d204224e214183a662828
Anson Wong Sie Yuan <awon0048@student.monash.edu>
2022-09-30 00:41:31 +0800

    added createTodo method in kanbanMain

--- Projects/kanbanMain.js
+++ Projects/kanbanMain.js
@@ -3,3 +3,40 @@ import Kanban from "./src/Kanban.js";
 new Kanban(
 	document.querySelector(".kanban")
 );
+
+function createTodo(task) {
+	const todo_div = document.createElement("div");
+	const txt = document.createTextNode(task)
+  //   const input_val = document.getElementById("todo_input").value;
+  //   const txt = document.createTextNode(input_val);
+  
+  
+	todo_div.appendChild(txt);
+	todo_div.classList.add("todo");
+	todo_div.setAttribute("draggable", "true");
+  
+	/* create span */
+	const span = document.createElement("span");
+	const span_txt = document.createTextNode("\u00D7");
+	span.classList.add("close");
+	span.appendChild(span_txt);
+  
+	todo_div.appendChild(span);
+  
+	no_status.appendChild(todo_div);
+  
+	span.addEventListener("click", () => {
+	  span.parentElement.style.display = "none";
+	});
+	//   console.log(todo_div);
+  
+	todo_div.addEventListener("dragstart", dragStart);
+	todo_div.addEventListener("dragend", dragEnd);
+  
+	document.getElementById("todo_input").value = "";
+	todo_form.classList.remove("active");
+	overlay.classList.remove("active");
+  }
+
+  
+  
