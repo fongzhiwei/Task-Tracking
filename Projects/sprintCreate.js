9a3fbda634e9ad4f013d59a7591354550cdb2cf5
zfon0005 <zfon0005@student.monash.edu>
2022-09-30 15:47:40 +0800

    link sprint board to sprint details

--- Projects/sprintCreate.js
+++ Projects/sprintCreate.js
@@ -2,7 +2,7 @@
 const sprintCreation = document.querySelector(".display");
 
 const generateNewCard = (sprint) => `<div class = "card">
-<div>
+<div id=${sprint.id} onclick ="buttonPressed()">
     <div id="upperLine">
         <h5 id="taskName"> <b> ${sprint.sprintTitle} </b></h5>
         <button type="button" class="btn btn-outline-danger" id=${sprint.id} class="asa" onclick="deleteTask.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
@@ -25,16 +25,16 @@ const generateNewCard = (sprint) => `<div class = "card">
 let globalStorage = [];
 
 // get task data from local storage
-const loadTaskData = () => {
-  const getTaskData = localStorage.getItem("sprint");
-  const {sprintTask} = JSON.parse(getTaskData);
+const loadSprintData = () => {
+  const getSprintData = localStorage.getItem("sprint");
+  const {sprintTask} = JSON.parse(getSprintData);
 
-  sprintTask.map((taskObject) => {
-    sprintCreation.insertAdjacentHTML("beforeend", generateNewCard(taskObject));
-    const b = document.getElementById(taskObject.id1);
-    b.innerText = localStorage.getItem(taskObject.id1);
+  sprintTask.map((sprintObject) => {
+    sprintCreation.insertAdjacentHTML("beforeend", generateNewCard(sprintObject));
+    const b = document.getElementById(sprintObject.id1);
+    b.innerText = localStorage.getItem(sprintObject.id1);
 
-    globalStorage.push(taskObject);
+    globalStorage.push(sprintObject);
     
   });
 
@@ -84,4 +84,22 @@ const statusButton = (event)=>{
    const button1 = document.getElementById(statusId);
    button1.innerText = localStorage.getItem(statusId);
 
+}
+
+const currentSprintDetails = () =>{
+
+}
+
+const buttonPressed = event => {
+  event = window.event;
+  var targetId = event.target.id;
+
+  if (/[a-z]/i.test(targetId)){
+    targetId = event.target.parentNode.id;
+  }
+
+  if (/[a-z]/i.test(targetId)){
+    targetId = event.target.parentNode.parentNode.id;
+  }
+  localStorage.setItem("sprintDetail", targetId)
 }
--- Projects/sprintDetails.js
+++ Projects/sprintDetails.js
@@ -1,4 +1,30 @@
 const openModal=()=>{
     const open = document.getElementById("myModal");
     open.style.display= "block";
+    loadSprintData();
+};
+
+// declaring an empty array to store data
+let globalStorage = [];
+
+// get task data from local storage
+const loadSprintData = () => {
+    const taskID = localStorage.getItem("sprintDetail")
+    const getSprintData = localStorage.getItem("sprint");
+    const {sprintTask} = JSON.parse(getSprintData);
+
+    sprintTask.map((sprintObject) => {
+        if(sprintObject.id == taskID){
+    
+            const sprintName = document.getElementById("sprintName");
+            console.log(sprintName.textContent)
+            sprintName.innerHTML = sprintObject.sprintTitle
+            const sprintStartDate = document.getElementById("sprintStartDate")
+            sprintStartDate.innerHTML = sprintObject.startDate
+            const sprintEndDate = document.getElementById("sprintEndDate")
+            sprintEndDate.innerHTML = sprintObject.endDate
+
+        }
+    });
+
 };
