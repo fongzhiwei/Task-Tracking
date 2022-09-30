c2ed155e610ae2df637d02ed62b34c77820f5072
syen9 <jamieyeow1203@gmail.com>
2022-09-30 14:38:47 +0800

    fixing create task and display

--- Projects/sprintCreate.js
+++ Projects/sprintCreate.js
@@ -1,22 +1,25 @@
 
 const sprintCreation = document.querySelector(".display");
 
-const generateNewCard = (sprint) => `<div class = "card">
-<div>
-    <div id="upperLine">
-        <h5 id="taskName"> <b> ${sprint.sprintTitle} </b></h5>
-        <button type="button" class="btn btn-outline-danger" id=${sprint.id} class="asa" onclick="deleteTask.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
-        <i class="fas fa-trash-alt"id=${sprint.id}
-        data-bs-target="#animateModal" data-bs-toggle="modal" onclick="deleteTask.apply(this, arguments)" ></i>
-      </button>
-      <div id="statusButtons"> 
-        <div><button id=${sprint.id1} onclick="statusButton.apply(this, arguments)">Get Started</button></div> 
-        <div><button type="button" id="finish" onclick="" > Finished </button> </div>
-         </div>
-        </div>
-        <div id="date" class="dateClass"><p id="date1">Start Date: ${sprint.startDate}</p> </div>
-        <div id="date2" class="dateClass"><p id="date3">End Date: ${sprint.endDate}</p> </div>
+const generateNewCard = (sprint) => `
+<div class = "card">
+  <div id="upperLine">
+    <h5 id="taskName"><b> ${sprint.sprintTitle} </b></h5>
+
+    <div class = "date">
+      <p id="date1">Start Date: ${sprint.startDate}</p>
+      <p id="date3">End Date: ${sprint.endDate}</p>
     </div>
+    
+    <div id="statusButtons"> 
+      <button type="button" class="btn btn-outline-danger" id=${sprint.id} class="asa" onclick="deleteTask.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
+        <i class="fas fa-trash-alt"id=${sprint.id} data-bs-target="#animateModal" data-bs-toggle="modal" onclick="deleteTask.apply(this, arguments)" ></i>
+      </button>
+
+      <div><button id=${sprint.id1} class = "started" onclick="statusButton.apply(this, arguments)">Get Started</button></div> 
+      <div><button class = "finished" type="button" id="finish" onclick=""> Finished </button></div>
+    </div>
+  </div> 
 </div>`
 
 ;
