d51ab2c1be2257939813a8f9ca92d29f23e6ee4d
zfon0005 <zfon0005@student.monash.edu>
2022-09-30 22:05:57 +0800

    task details bug fixes

--- Projects/taskDetails.js
+++ Projects/taskDetails.js
@@ -6,13 +6,13 @@ const openTaskModal=()=>{
 
 // get task data from local storage
 const loadTaskDetail = () => {
-    const taskID = localStorage.getItem("sprintDetail")
+    const taskID = localStorage.getItem("taskDetail")
     const getTaskData = localStorage.getItem("task");
-    const {taskData} = JSON.parse(getTaskData);
-    console.log(taskData)
+    const {task} = JSON.parse(getTaskData);
 
-    taskData.map((taskObject) => {
+    task.map((taskObject) => {
         if(taskObject.id == taskID){
+            console.log(taskObject)
     
             const name = document.getElementById("name");
             name.innerText = taskObject.taskName
