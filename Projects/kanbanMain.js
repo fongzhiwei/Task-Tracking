9d51e100adb4880919ece4cfefb9a65c5a8b05f6
Anson Wong Sie Yuan <awon0048@student.monash.edu>
2022-10-01 00:14:21 +0800

    added generateNewSprint method

--- Projects/kanbanMain.js
+++ Projects/kanbanMain.js
@@ -4,9 +4,38 @@ new Kanban(
 	document.querySelector(".kanban")
 );
 
-function createSprint(task) {
-	if(taskObject.taskStatus == "To Do Task"){
+const generateNewSprint = (taskData) => `
+<div class ="displayTaskSlot">
+  <div class="col-md-6 col-lg-4 mt-3" >
+    <div class="card shadow-sm task__card">
 
+      <div class="card-header d-flex justify-content-end task__card__header">
+      </div>
+
+      <div class="card-body">
+        <h2 class="task__card__name" id="name">${taskData.taskName}
+        </h2>
+          <form>
+            <div class="tags text-white flex-wrap"><span class="badge bg-primary m-1" style="background-color: red"; id="priority">${taskData.taskPriority}</span>
+            <div class="tags text-white flex-wrap"><span class="badge bg-success m-1" id="story">${taskData.storyPoint}</span></div>
+          </form>
+
+          <form>
+            <div class="tags text-white flex-wrap"><span class="badge bg-success m-1" id="type">${taskData.taskType}</span></div>
+            <div class="tags text-white flex-wrap"><span class="badge bg-primary m-1" id="status">${taskData.taskStatus}</span>
+          </form>
+      </div>
+    </div>
+  </div>
+</div>
+`;
+
+function createSprint(task) {
+
+	if(taskObject.taskStatus == "To Do Task"){
+		stat = task.getElementById("taskStatus")
+		generateNewSprint(stat)
+		
 	}
 	else if (taskObject.taskStatus == "In Progress"){
 
