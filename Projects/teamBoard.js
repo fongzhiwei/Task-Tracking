5649f8bf06084470d2ad0688313df7ca826bcc6a
Ng Yu Mei <yngg0068@student.monash.edu>
2022-10-11 14:21:46 +0800

    add function to display member added in team board

---
+++ Projects/teamBoard.js
@@ -0,0 +1,68 @@
+const TeamAdding = document.querySelector(".display");
+
+const generateNewCard = (member) => `
+<div class = "card" id=${member.id}>
+  <div id="upperLine">
+        <p id="name">Member Name: ${member.name}</p>
+      
+        <p id="Email">Member Name: ${member.address}</p>
+      
+      <div id="statusButtons"> 
+        <button type="button" class="btn btn-outline-danger" id=${member.id} class="asa" onclick="deleteMember.apply(this, arguments)" data-bs-target="#animateModal" data-bs-toggle="modal" >
+          <i class="fas fa-trash-alt"id=${member.id} data-bs-target="#animateModal" data-bs-toggle="modal" onclick="deleteMember.apply(this, arguments)" ></i>
+        </button>
+      </div>
+    </div>
+</div>`
+;
+
+// declaring an empty array to store data
+let globalStorage = [];
+
+// get task data from local storage
+const loadTeamData = () => {
+  const getTeamData = localStorage.getItem("team");
+  const {teamMembers} = JSON.parse(getTeamData);
+
+  teamMembers.map((member) => {
+    TeamAdding.insertAdjacentHTML("beforeend", generateNewCard(member));
+    globalStorage.push(member);
+    
+  });
+
+};
+
+
+// save changes of new task 
+const saveChanges = () => {
+    const teamData = {
+      id: Date.now(),
+      name: document.getElementById("memberName").value, 
+      address: document.getElementById("email").value,
+    };
+    TeamAdding.insertAdjacentHTML("beforeend", generateNewCard(teamData));
+  
+    globalStorage.push(teamData);
+    localStorage.setItem("team", JSON.stringify({teamMembers: globalStorage}));
+  };
+
+
+  const deleteMember = (event) => {
+    event = window.event;
+    const targetId = event.target.id;
+    const tagName = event.target.tagName;
+  
+    globalStorage = globalStorage.filter(
+      (member) => member.id != targetId
+    );
+  
+    localStorage.setItem("team", JSON.stringify({teamMembers: globalStorage}));
+  
+    if (tagName == "BUTTON") {
+      return TeamAdding.removeChild(
+        event.target.parentNode.parentNode.parentNode
+      );
+    } else {
+      return TeamAdding.removeChild(event.target.parentNode.parentNode.parentNode.parentNode);
+    }
+  };
