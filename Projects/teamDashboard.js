cee77137460da764c7d9c0c05d4ecb5dc7b929b0
Anson Wong Sie Yuan <awon0048@student.monash.edu>
2022-10-17 00:27:17 +0800

    Added generateNew Section and loadMemberDetail

--- Projects/teamDashboard.js
+++ Projects/teamDashboard.js
@@ -1,7 +1,35 @@
+teamDetail = document.querySelector("[id=memberList]")
+
 const openMemberModal=()=>{
     const open = document.getElementById("myModal");
     open.style.display= "block";
     loadTaskDetail();
 };
 
+
+
+const generateNewSection = (member,count) =>`
+<tr>
+<td>${count}</td>
+<td>${member.name}</td>
+<td>${member.address}</td>
+<td>3 Hours</td>
+</tr>`
+;
+
+
 let globalStorage = [];
+
+const loadMemberDetail = () => {
+    const getMemberData = localStorage.getItem("team");
+    const {team} = JSON.parse(getMemberData);
+    i = 0;
+    team.map((memberData) => {
+        globalStorage.push(memberData)
+        
+
+        generateNewSection(memberData,count);
+        count++;
+    });
+
+};
